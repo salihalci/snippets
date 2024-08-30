892ea5ccd3c6a8b932e10078bcd97ae68e473b16
salci <salihalci@gmail.com>
2024-08-30 23:03:16 +0300

    noft found added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
-import Image from "next/image";
 
-export default function Home() {
-  return <div>HomePage</div>;
+import {db} from '@/db';
+
+export default async function Home() {
+
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet)=>{
+    return (
+      <div key ={snippet.id}>
+        {snippet.title}
+      </div>
+    );
+  })
+
+
+  return <div>{renderedSnippets}</div>
+  
+  
   
 }
---
+++ app/snippets/[id]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function SnippetNotFound(){
+    return <div>
+        <h1 className="text-xl bold">
+
+            Sorry, we could not find the records..
+        </h1>
+    </div>
+}
